74b6eb0a12f03218515e388c74c4e501cb220313
Maya Saxena <maya.saxena@tufts.edu>
2016-04-22 00:18:10 -0400

    Started to add patient lookup

---
+++ RefugeeDatabase/PatientLookupViewController.h
@@ -0,0 +1,13 @@
+//
+//  PatientLookupViewController.h
+//  RefugeeDatabase
+//
+//  Created by Maya Saxena on 4/22/16.
+//  Copyright © 2016 Workinonit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PatientLookupViewController : UIViewController
+
+@end
---
+++ RefugeeDatabase/PatientLookupViewController.m
@@ -0,0 +1,13 @@
+//
+//  PatientLookupViewController.m
+//  RefugeeDatabase
+//
+//  Created by Maya Saxena on 4/22/16.
+//  Copyright © 2016 Workinonit. All rights reserved.
+//
+
+#import "PatientLookupViewController.h"
+
+@implementation PatientLookupViewController
+
+@end
